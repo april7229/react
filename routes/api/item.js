4c3ea4f5e472a441610774c717ebdd82f32c8ae2
april <martinezapril56@yahoo.com>
2019-01-10 20:01:23 -0500

    mern

--- routes/api/item.js
+++ routes/api/item.js
@@ -12,8 +12,22 @@ router.get( '/', ( req, res ) =>
     Item.find()
         .sort( { date: -1 } )
         .then( items => res.json( items ) );
-});
+} );
 
+router.post( '/', ( req, res ) =>
+{
+    const newItem = new Item( {
+        name: req.body.name
+    } );   
+    
+    newItem.save().then( item => res.json(item));
+} );
+
+router.delete( '/:id',( req, res ) => {
+    Item.findById( req.params.id )
+        .then( item => item.remove().then( () => res.json( { success: true })))
+    
+})
 
 
 module.exports = router;
